5b8f57c851646e8637d878723348ae19cbe2ed74
Imran <imufun1@gmail.com>
2015-10-24 01:40:18 +0600

    new add

--- app/src/main/java/com/example/imran/dhakaweather/MainActivity.java
+++ app/src/main/java/com/example/imran/dhakaweather/MainActivity.java
@@ -10,8 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
@@ -25,15 +25,50 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
     private CurrentWeather mCurrentWeather;
+
+    TextView mTimeLable, mhumidityLable, mlocation, mprecipLable, msummerLable, MtempretureLable;
+    ImageView miconimageView;
+
+
+//
+//    @Bind(R.id.Timelable)
+//    TextView mTimeLable;
+//    @Bind(R.id.humidityLable)
+//    TextView mhumidityLable;
+//    @Bind(R.id.iconimageView)
+//    TextView miconimageView;
+//    @Bind(R.id.location)
+//    TextView mlocation;
+//    @Bind(R.id.precipLable)
+//    TextView mprecipLable;
+//    @Bind(R.id.summerLable)
+//    TextView msummerLable;
+//    @Bind(R.id.tempretureLable)
+    //  TextView MtempretureLable;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+
+        MtempretureLable = (TextView) findViewById(R.id.tempretureLable);
+        mTimeLable = (TextView) findViewById(R.id.Timelable);
+        mhumidityLable = (TextView) findViewById(R.id.humidityLable);
+        miconimageView = (ImageView) findViewById(R.id.iconimageView);
+        mprecipLable = (TextView) findViewById(R.id.precipLable);
+        mlocation = (TextView) findViewById(R.id.location);
+        msummerLable = (TextView) findViewById(R.id.summerLable);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -45,11 +80,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-    String apiKeys = "c73f4b46758d0409de27820e355e733b";
-    double LATITUDE = 23.7805733;
-    double LONGITUDE = 90.279237;
-    String forcastUrl = "https://api.forecast.io/forecast/" + apiKeys + "/" + LATITUDE + "," + LONGITUDE;
+        String apiKeys = "c73f4b46758d0409de27820e355e733b";
+        double LATITUDE = 23.7805733;
+        double LONGITUDE = 90.279237;
+        String forcastUrl = "https://api.forecast.io/forecast/" + apiKeys + "/" + LATITUDE + "," + LONGITUDE;
 
         if (isNetworkAvilable()) {
 
@@ -71,6 +105,13 @@ public class MainActivity extends AppCompatActivity {
                         Log.v(TAG, jsondata);
                         if (response.isSuccessful()) {
                             mCurrentWeather = getCurrentDetalis(jsondata);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    UpdateDisplay();
+                                }
+                            });
+
                         } else {
                             alertDialogAboutError();
                         }
@@ -86,7 +127,17 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, R.string.network_error, Toast.LENGTH_LONG).show();
         }
 
-}
+    }
+
+    private void UpdateDisplay() {
+        MtempretureLable.setText(mCurrentWeather.getmTeamperature() + "");
+        mTimeLable.setText("At" + mCurrentWeather.getFormattedTime() + " It will be");
+        mhumidityLable.setText(mCurrentWeather.getmHumidity() + "");
+        mprecipLable.setText(mCurrentWeather.getmPrecipChance() + "");
+        msummerLable.setText(mCurrentWeather.getmSummary());
+
+
+    }
 
     private CurrentWeather getCurrentDetalis(String jsondata) throws JSONException {
         JSONObject forecase = new JSONObject(jsondata);
@@ -126,7 +177,6 @@ public class MainActivity extends AppCompatActivity {
         dialog.show(getFragmentManager(), "error Dialog");
 
     }
-
 
 
 }
